7d1d4caf9d91f80e8a3e1ca33f47a5b17fad4153
CS7player <144120363+CS7player@users.noreply.github.com>
2024-12-26 22:16:23 +0530

    changes

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const app = express();
 app.use(express.json());
 const cors = require('cors');
 const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
-app.use((req, res, next) => {
- const origin = req.headers.origin || 'default';
-//  console.log("origin", origin)
- if (!req.headers.origin || allowedOrigins.includes(origin)) {
-  res.setHeader('Access-Control-Allow-Origin', origin || '*');
- }
- res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
- res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
- res.setHeader('Access-Control-Allow-Credentials', 'true');
- next();
-});
+// app.use((req, res, next) => {
+//  const origin = req.headers.origin || 'default';
+// //  console.log("origin", origin)
+//  if (!req.headers.origin || allowedOrigins.includes(origin)) {
+//   res.setHeader('Access-Control-Allow-Origin', origin || '*');
+//  }
+//  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+//  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//  res.setHeader('Access-Control-Allow-Credentials', 'true');
+//  next();
+// });
 const routes = require('./app/routes');
 app.use('/', routes);
 const { mongoConnect } = require('./app/utils/db');
